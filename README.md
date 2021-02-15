1180d43ac38bb0a33446e6afcc7c3a9e977b23f6
Joeri Nicolaes <joerinicolaes@gmail.com>
2021-02-15 08:54:25 +0100

    Skeleton code for python script to process a video with OpenCV2 > python script takes 2 inputs (see --help) to load an input video file, process it frame by frame and save the results to output video file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # opencv_video
-frame by frame video processing using open cv
+This repository contains python code snippets for frame by frame video processing using opencv2.
+
+# Getting Started
+1. Clone repo
+2. Download or capture a video file that you that you would like to process (using your webcam and favorite video capture tool such as Quicktime)
+3. Adapt main() function in opencv_process_video script with opencv2 functions to process selected frames
+4. Run the main script
+    ```shell script
+   $ python opencv_process_video.py -i <PATH_TO_INPUT_VIDEO_FILE> -o <PATH_TO_OUPUT_VIDEO_FILE>
+    ``` 
---
+++ opencv_process_video.py
@@ -0,0 +1,66 @@
+""" Skeleton code for python script to process a video with OpenCV2
+
+:copyright: (c) 2021, Joeri Nicolaes
+:license: BSD license
+"""
+import argparse
+import cv2
+import sys
+
+
+# helper function to change what you do based on video seconds
+def between(cap, lower: int, upper: int):
+    return lower <= int(cap.get(cv2.CAP_PROP_POS_MSEC)) < upper
+
+
+def main(input_video_file: str, output_video_file: str) -> None:
+    # opencv video objects to work with
+    cap = cv2.VideoCapture(input_video_file)
+    fps = int(round(cap.get(5)))
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    out = cv2.VideoWriter(output_video_file, fourcc, fps, (frame_width, frame_height))
+
+    # while loop where the real work happens
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            if cv2.waitKey(28) & 0xFF == ord('q'):
+                break
+            if between(cap, 0, 500):
+                # do something using opencv functions (skipped here so we simply write the input frame back to output)
+                pass
+            # ...
+
+            # write frame that you processed to output
+            out.write(frame)
+
+            # (optional) display the resulting frame
+            cv2.imshow('Frame', frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release the video capture object
+    cap.release()
+    out.release()
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='OpenCV video processing')
+    parser.add_argument('-i', "--input", help='full path to input video that will be processed')
+    parser.add_argument('-o', "--output", help='full path for saving processed video output')
+    args = parser.parse_args()
+
+    if args.input is None or args.output is None:
+        sys.exit("Please provide path to input and output video files! See --help")
+
+    main(args.input, args.output)
